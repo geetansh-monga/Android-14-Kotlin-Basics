5b7ea6555c78640e18435a059d715fb5b25bfd95
Geetansh Monga <gmgog0402@gmail.com>
2024-07-01 00:49:58 +0530

    Boolean Practice

---
+++ app/src/main/java/com/example/kotlinbasics/BooleansPractice.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbasics
+
+fun main(){
+    var myTrue = true
+    var myFalse = false
+
+    println(myTrue || myFalse) // OR Operator
+    println(myTrue && myFalse) // AND Operator
+    println(!myTrue) // NOT Operator
+
+}
