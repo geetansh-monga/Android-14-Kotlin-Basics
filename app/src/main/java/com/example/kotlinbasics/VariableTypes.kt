ca022a07bcf373b4eba731fbfc31bc67ddce9d39
Geetansh Monga <gmgog0402@gmail.com>
2024-07-01 00:27:38 +0530

    Variable Types

--- app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
+++ app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -1,8 +1,8 @@
 package com.example.kotlinbasics
 
 fun main(){
-    val constantNumber:Int = 44;
-    var mutableString:String = "Geetansh";
+    val constantNumber = 44
+    var mutableString = "Geetansh"
     println("Constant Number: $constantNumber")
     println("Mutable String: $mutableString")
     mutableString = "Geetansh Monga"
