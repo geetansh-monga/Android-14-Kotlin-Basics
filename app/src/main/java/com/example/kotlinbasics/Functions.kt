c3c971f623e6fe08e3c42905fcea321521ab880f
Geetansh Monga <gmgog0402@gmail.com>
2024-07-02 00:43:05 +0530

    Functions

---
+++ app/src/main/java/com/example/kotlinbasics/Functions.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+
+fun main(){
+    makeCoffee()
+    makeCoffee()
+    makeCoffee()
+}
+
+fun makeCoffee(){
+    println("Buy Beans")
+    println("Boil water to 90 degree celsius")
+    println("grind the beans")
+    println("push the hot water through grinded beans")
+    println("Pour the coffee in the mug")
+}
