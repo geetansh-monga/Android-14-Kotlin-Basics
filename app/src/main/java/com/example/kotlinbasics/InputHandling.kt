f2aa6cbe882a9355ee7ded77e8938e43e2aed054
Geetansh Monga <gmgog0402@gmail.com>
2024-08-06 23:56:32 +0530

    cleaned kotlin basics exercises from main.

--- app/src/main/java/com/example/kotlinbasics/InputHandling.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinbasics
-
-fun  main(){
-    println("Please enter a number: ")
-    val inputString = readln()
-    val inputNumber = inputString.toInt()
-    val multiplier = 5
-    println("result after multiplication is ${inputNumber*multiplier}")
-}
